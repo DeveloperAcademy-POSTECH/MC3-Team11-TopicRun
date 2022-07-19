8a7903fab42db480979f675c549f023ee90f489b
Shin-jun <inside1451@naver.com>
2022-07-19 15:21:56 +0900

    [FEAT] StartPageViewController 추가
    StartPageView 스토리보드, StartPageViewController를 추가하였습니다.

---
+++ TopicRun/TopicRun/StartPageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  StartPageViewController.swift
+//  TopicRun
+//
+//  Created by Shin yongjun on 2022/07/19.
+//
+
+import Foundation
+import UIKit
+
+class StartPageViewController: UIViewController {
+    override func viewDidLoad() {
+        print("11")
+    }
+}
