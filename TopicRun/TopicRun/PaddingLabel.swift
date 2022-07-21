3d41875a6b99efe796a3403303f500ff5b6a3108
Jedi <shwoghk14@gmail.com>
2022-07-21 16:38:01 +0900

    [FEAT] 레이블 패딩 추가.

---
+++ TopicRun/TopicRun/PaddingLabel.swift
@@ -0,0 +1,24 @@
+//
+//  PaddingLabel.swift
+//  TopicRun
+//
+//  Created by Jaehwa Noh on 2022/07/21.
+//
+
+import Foundation
+import UIKit
+
+class PaddingLabel: UILabel {
+
+    var edgeInset: UIEdgeInsets = .zero
+
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets.init(top: edgeInset.top, left: edgeInset.left, bottom: edgeInset.bottom, right: edgeInset.right)
+        super.drawText(in: rect.inset(by: insets))
+    }
+
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + edgeInset.left + edgeInset.right, height: size.height + edgeInset.top + edgeInset.bottom)
+    }
+}
